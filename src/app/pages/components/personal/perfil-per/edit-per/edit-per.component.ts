0df4ea55356760f10822636e41721b898ed569b0
Bairon Yasith Vega Martínez <bavegama@est.utn.ac.cr>
2024-08-25 18:16:34 -0600

    Avance Bairon Vega

---
+++ src/app/pages/components/personal/perfil-per/edit-per/edit-per.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-per',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-per.component.html',
+  styleUrl: './edit-per.component.css'
+})
+export class EditPerComponent {
+
+}
---
+++ src/app/pages/components/personal/perfil-per/perfil-per.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-per',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-per.component.html',
+  styleUrl: './perfil-per.component.css'
+})
+export class PerfilPerComponent {
+
+}
