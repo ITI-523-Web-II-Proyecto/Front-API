afe20c818635657aa8d5ccf2b3b4d42435e17798
Bairon Yasith Vega Martínez <bavegama@est.utn.ac.cr>
2024-08-25 18:28:23 -0600

    Avance Bairon Vega

--- src/app/pages/components/personal/perfil-per/perfil-per.component.ts
+++ src/app/pages/components/personal/perfil-per/perfil-per.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-per',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,],
   templateUrl: './perfil-per.component.html',
   styleUrl: './perfil-per.component.css'
 })
